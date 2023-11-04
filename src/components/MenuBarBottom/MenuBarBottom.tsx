34f262dd4754ace2c0ec73b7fe77bfce78116cc9
Kamil Lewucha <lewucha2000@gmail.com>
2023-11-05 00:30:02 +0100

    Added menu position for user login and user profile (only if user is logged in), uploaded additonal icons for several menu items

--- src/components/MenuBarBottom/MenuBarBottom.tsx
+++ src/components/MenuBarBottom/MenuBarBottom.tsx
@@ -1,11 +1,17 @@
+import { Box } from "@chakra-ui/react";
 import { MenuBarElement } from "../MenuBarElement/MenuBarElement";
 
 import "./MenuBarBottom.css";
+import { useState } from "react";
 
 export const MenuBarBottom = () => {
+  const [isLogged, setIsLogged] = useState<boolean>(true);
+
   return (
-    <div className="menu__bottom">
-      <MenuBarElement path="/addRestaurant/form" text="Add Restaurant" />
-    </div>
+    <Box className="menu__top">
+      <MenuBarElement path="/addRestaurant/form" text="Add Restaurant" iconSrc="/icons/add-circle.icon.svg"/>
+      {isLogged && <MenuBarElement path="/user" text="Profile" iconSrc="/icons/user.icon.svg"/>}
+      {!isLogged && <MenuBarElement path="/login" text="Login" iconSrc="/icons/login.icon.svg"/>}
+    </Box>
   );
 };
